c1aff8ba6f4abbb13545779dcf3fbd34cb2e0094
Logan Stewart <stewart.jlt@gmail.com>
2017-01-31 23:15:34 -0500

    Roll a Ball Unity Demo
    Unity Tutorial

---
+++ Assets/Scipts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+        offset = transform.position - player.transform.position; // get the offset between the camera and player so we can do third person camera things
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {  //dont do camera things here, its not garunteed to be run propperlly, it will do the updating before players can see things
+        transform.position = player.transform.position;
+	}
+
+    void LateUpdate() //does things after all things in Update are done! good for camera, follow animations, gathering procedural states
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scipts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+
+	
+    void Start()
+    {
+       
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void Update() //called before rendering a frame, good for game code
+    {
+
+    }
+
+    void FixedUpdate()// called before physics calculations, good for physics related things
+    {
+        
+        float moveHorizontal =  Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+      
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject); //kills gameobject we come in contact with
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "YOU WIN!";
+        }
+    }
+
+    
+}
